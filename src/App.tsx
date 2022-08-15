d2faea0a2c9c5986910ed70a699abf127341f40c
JuanDonito <anguadir@gmail.com>
2022-08-15 02:37:29 +0200

    added main to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header.component";
+import Main from "./components/Main.component";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <Main />
     </React.Fragment>
   );
 }
