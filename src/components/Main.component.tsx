052f59aac8716d67e46fcd69bac18f30df386bc3
JuanDonito <anguadir@gmail.com>
2022-08-21 21:14:31 +0200

    refactor: moved header to main

--- src/components/Main.component.tsx
+++ src/components/Main.component.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from "react";
 import StyledMain from "./Main.styled";
 import Clock from "./Clock.component";
+import Header from "./Header.component";
 
 const Main: FC = () => {
   return (
     <StyledMain>
+      <Header />
       <Clock />
     </StyledMain>
   );
