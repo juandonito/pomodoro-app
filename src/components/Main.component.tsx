890053b2038cf04c89df16f149423e8946f955a2
JuanDonito <anguadir@gmail.com>
2022-08-22 21:42:00 +0200

    feature(navigation): added navigation component to main

--- src/components/Main.component.tsx
+++ src/components/Main.component.tsx
@@ -2,11 +2,13 @@ import React, { FC } from "react";
 import StyledMain from "./Main.styled";
 import Clock from "./Clock.component";
 import Header from "./Header.component";
+import Navigation from "./Navigation.component";
 
 const Main: FC = () => {
   return (
     <StyledMain>
       <Header />
+      <Navigation />
       <Clock />
     </StyledMain>
   );
