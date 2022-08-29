ccd932ae90d89f1be922593e15fada699103f768
JuanDonito <anguadir@gmail.com>
2022-08-29 20:50:14 +0200

    feature(navigation): created routes

--- src/components/Main.component.tsx
+++ src/components/Main.component.tsx
@@ -1,15 +1,31 @@
 import React, { FC } from "react";
+import { useRoutes } from "react-router-dom";
+
 import StyledMain from "./Main.styled";
 import Clock from "./Clock.component";
 import Header from "./Header.component";
 import Navigation from "./Navigation.component";
 
 const Main: FC = () => {
+  const routes = useRoutes([
+    {
+      path: "/shortbreak",
+      element: <Clock time={5} />,
+    },
+    {
+      path: "/longbreak",
+      element: <Clock time={10} />,
+    },
+    {
+      path: "/*",
+      element: <Clock time={25} />,
+    },
+  ]);
   return (
     <StyledMain>
       <Header />
       <Navigation />
-      <Clock />
+      {routes}
     </StyledMain>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import theme from "./_styles/theme";
 
@@ -12,10 +13,12 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
